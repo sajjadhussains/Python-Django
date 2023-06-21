54cf313775dbb998776b3fd380afe72dc99d2039
sajjadhussains <shuvosajjad70@gmail.com>
2023-06-22 03:59:01 +0600

    after completing django template

--- third_project/first_app/views.py
+++ third_project/first_app/views.py
@@ -3,4 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def contact(request):
-    return render(request,'./first_app/index.html',context={'author':'sajjad','age':17,'cgpa':2.8})
+    return render(request,'./first_app/index.html',context={'author':'sajjad','age':17,'cgpa':2.8,
+                                                            'courses':[
+                                                                {
+                                                                    'id':1,
+                                                                    'course':'C',
+                                                                    'teacher':'sajjad'
+                                                                },
+                                                                {
+                                                                   'id':2,
+                                                                   'course':'javascript',
+                                                                   'teacher':'hussain' 
+                                                                },
+                                                                {
+                                                                    'id':3,
+                                                                    'course':'Python',
+                                                                    'teacher':'Shuvo'
+                                                                }
+                                                                ],})
--- third_project/third_project/settings.py
+++ third_project/third_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'third_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR / "static"
+]
+MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
