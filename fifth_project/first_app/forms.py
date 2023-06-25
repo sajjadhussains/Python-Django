d59a33896e86e31fc57880c3bb53e0a90e7fb7e1
sajjadhussains <shuvosajjad70@gmail.com>
2023-06-25 06:36:30 +0600

    some changes

--- fifth_project/first_app/forms.py
+++ fifth_project/first_app/forms.py
@@ -1,15 +1,20 @@
 from django import forms 
 
+# widgets == field to html input
 class contactForm(forms.Form):
-    name = forms.CharField(label='User Name')
+    name = forms.CharField(label='User Name',initial='Rahim',help_text="total character must be at least 70 characters",required=False,widget=forms.Textarea)
+    # file = forms.FileField()
     email = forms.EmailField(label='User Email')
     age = forms.IntegerField()
     weight = forms.FloatField()
     balance = forms.DecimalField()
     check = forms.BooleanField()
-    birthday = forms.DateField()
-    appointment = forms.DateTimeField()
+    # birthday = forms.DateField(widget=forms.DateInput(attrs={'type':'Date'}))
+    # appointment = forms.DateTimeField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
+    # if DateField is not remembered then charField can use
+    birthday = forms.CharField(widget=forms.DateInput(attrs={'type':'Date'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
     CHOICES=[('S','small'),('M','Medium'),('L','Large')]
-    size=forms.ChoiceField(choices=CHOICES)
+    size=forms.ChoiceField(choices=CHOICES,widget=forms.RadioSelect)
     MEAL=[('P','Pappermoni'),('M','Mashroom'),('B','Beef')]
-    pizza = forms.MultipleChoiceField(choices=MEAL)
+    pizza = forms.MultipleChoiceField(choices=MEAL,widget=forms.CheckboxSelectMultiple)
